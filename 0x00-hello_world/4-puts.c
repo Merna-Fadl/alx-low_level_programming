019774cddbebcaa8c4449764f1182643e9f85119
Merna Fadl <merna.fadl1996@gmail.com>
2023-07-13 22:53:58 +0300

    4-puts.c project

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * return:always 0 success
- * /
-int main (void)
+ * main-entry point
+ * return: always 0
+ */
+
+int main(void)
 {
-	puts("\"ogramming is like building a multilingual puzzle\n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
