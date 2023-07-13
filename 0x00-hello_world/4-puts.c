6bb88a8c62f5f624b60cc11cc072a18f2fa323b8
Merna Fadl <merna.fadl1996@gmail.com>
2023-07-13 22:40:48 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * return:always 0 success
+ * /
 int main (void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"ogramming is like building a multilingual puzzle\n");
 	return 0;
 }
-
