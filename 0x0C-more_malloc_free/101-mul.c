b55a91edc8c155ab529e1db39c3511f973961f86
Merna Fadl <merna.fadl1996@gmail.com>
2023-08-11 00:59:08 +0300

    101-mul.c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib>
+#include "main.h"
+/**
+ *multiple -  program that multiplies two positive numbers.
+ * mul: mult
+ * num1: number
+ * num2: number
+ * return: 0
+ */
