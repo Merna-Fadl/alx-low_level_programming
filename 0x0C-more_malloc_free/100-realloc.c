c6c048a7dac02493203acddab3b106666fdd7b55
Merna Fadl <merna.fadl1996@gmail.com>
2023-08-11 00:49:48 +0300

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,31 +10,33 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *p;
 	unsigned int i;
+	char *ptr1;
+	char *old_ptr;
 
 	if (new_size == old_size)
 		return (ptr);
-	if (new_size == 0 && ptr != NULL)
+	if (new_size == 0 && ptr)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	if (ptr == NULL)
+	if (!ptr)
+		return (malloc(new_size));
+	ptr1 = malloc(new_size);
+	if (!ptr1)
+		return (NULL);
+	old_ptr = ptr;
+	if (new_size < old_size)
 	{
-		p = malloc(new_size);
-		if (p == NULL)
-			return (NULL);
-		return (p);
+		for (i = 0; i < new_size; i++)
+			ptr1[i] = old_ptr[i];
 	}
 	if (new_size > old_size)
 	{
-		p = malloc(new_size);
-		if (p == NULL)
-			return (NULL);
-		for (i = 0; i < old_size && i < new_size; i++)
-			*((char *)p + i) = *((char *)ptr + i);
-		free(ptr);
+		for (i = 0; i < old_size; i++)
+			ptr1[i] = old_ptr[i];
 	}
+	free(ptr);
 	return (0);
 }
