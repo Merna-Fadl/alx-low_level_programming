212bcca7167369953060be4ba71393a88f91ee00
Merna-Fadl <merna.fadl1996@gmail.com>
2023-08-23 20:00:26 +0300

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - it all start here
+ * @argc: num of arg
+ * @argv: array of pointer
+ * Return: 0
+ */
+int main(int argc, char const *argv[])
+{
+	int sum = 0;
+	char *c;
+
+	while (--argc)
+	{
+		for (c = argv[argc]; *c; c++)
+			if (*c < '0' || *c > '9')
+				return (printf("Error\n"), 1);
+		sum += atoi(argv[argc]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
