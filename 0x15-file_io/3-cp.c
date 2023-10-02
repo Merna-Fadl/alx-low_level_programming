646fd011a8afffe3b262ca4cf56d1344633d2f0a
Merna-Fadl <merna.fadl1996@gmail.com>
2023-10-02 22:19:26 +0300

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#define USAGE "Usage: cp file_from file_to %s\n"
+#define USAGE "Usage: cp file_from file_to\n"
 #define ERR_NOREAD "rror: Can't read from file %s\n"
 #define ERR_NOWRITE "Error: Can't write to %s\n"
 #define ERR_NOCLOSE "Error: Can't close fd %d\n"
