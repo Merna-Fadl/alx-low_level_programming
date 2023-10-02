f46592a363b4dcde5097dab1b701ce9efd2a18a8
Merna-Fadl <merna.fadl1996@gmail.com>
2023-10-02 22:13:53 +0300

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#define USAGE "Usage: cp file_from file_to\n"
+#define USAGE "Usage: cp file_from file_to %s\n"
 #define ERR_NOREAD "rror: Can't read from file %s\n"
 #define ERR_NOWRITE "Error: Can't write to %s\n"
 #define ERR_NOCLOSE "Error: Can't close fd %d\n"
@@ -35,6 +35,6 @@ int main(int ac, char **av)
 	if (from_fd)
 		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
 	if (to_fd)
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, to_fd), exit(100);
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
 return (EXIT_SUCCESS);
 }
